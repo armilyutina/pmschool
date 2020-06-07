863d5aa8b0fb7a297094c92bd614bbead891f639
Alina <bogdanovic2014@yandex.ru>
2020-06-07 22:13:50 +0300

    third-commit

--- pmschool/settings.py
+++ pmschool/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&vbywdt^#r$_ll1g3*e4psh%c^$bdpd9she-g)d(h%v!xgipd5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['phmschool.herokuapp.com','127.0.0.1']
 
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
